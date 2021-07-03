93428e721c96cc3887866ef2ded66dfe8ef4ff40
Ashley <liweimei0701@gmail.com>
2021-07-03 18:18:59 +1000

    add routes and create the route in app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,53 @@
+// React
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
+// Defining routes
+import * as ROUTES from "./routes";
 
-function App() {
-  return (
-   
-  );
-}
-
+const App = () => {
+	return (
+		<Switch>
+			<PublicRoute
+				exact
+				path={ROUTES.SETTING}
+				component={ROUTES.Setting}
+				title={ROUTES.SETTINGTITLE}
+			/>
+				<PublicRoute
+				exact
+				path={ROUTES.SETTINGPAGES}
+				component={ROUTES.SettingPages}
+				title={ROUTES.SETTINGPAGESTITLE}
+			/>
+			<PublicRoute
+				exact
+				path={ROUTES.SETTINGUSEQUERRY}
+				component={ROUTES.SettingUseQuerry}
+				title={ROUTES.SETTINGQUERRYTITLE}
+			/>
+			{/* Handles not found routes */}
+			<PublicRoute
+				path="*"
+				component={ROUTES.NotFound}
+				title={ROUTES.NOTFOUNDTITLE}
+			/>
+		</Switch>
+	);
+};
 export default App;
+
+const PublicRoute = ({
+	component: Component,
+	title,
+	...rest
+}) => {
+	return (
+		<Route
+			{...rest}
+			render={(props) => (
+					<Component {...props} title={title} />
+			)}
+		/>
+	);
+};
---
+++ src/routes.js
@@ -0,0 +1,17 @@
+// Define components path
+export { default as SettingUseQuerry } from "pages/Settings/settingUseQuerry";
+export { default as Setting } from "pages/Settings/SettingHome/setting";
+export { default as SettingPages } from "pages/Settings/SettingPages/settingPages";
+
+
+// Define routes path
+export const SETTINGUSEQUERRY = "/setting-usequerry";
+export const SETTING = "/setting";
+export const SETTINGPAGES = "/setting-pages/:id";
+
+
+// Define Route Title
+export const SETTINGQUERRYTITLE = "SETTINGQUERRY";
+export const SETTINGTITLE = "SETTING";
+export const SETTINGPAGESTITLE = "SETTINGPAGES";
+
